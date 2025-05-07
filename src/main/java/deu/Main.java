1fdf5c590b45bd151b3646e29d6dfc3bc8ae5a00
oxxultus <a38027928@gmail.com>
2025-05-07 10:07:31 +0900

    update annotation

--- src/main/java/deu/Main.java
+++ src/main/java/deu/Main.java
@@ -6,7 +6,7 @@ import deu.view.Auth;
 public class Main {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(() -> {
-            Auth authView = new Auth(); // View 생성
+            Auth authView = new Auth(); // authView 생성
             new AuthSwingController(authView); // Controller가 View에 이벤트 리스너 연결
             authView.setVisible(true); // 창 띄우기
         });
